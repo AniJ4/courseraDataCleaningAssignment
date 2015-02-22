2e7d8b752b1b8646ca6d7169102880d789f36b29
Anirudh Joshi <j.anirudha@gmail.com>
2015-02-22 18:13:15 +0530

    First Commit

---
+++ run_analysis.R
@@ -0,0 +1,9 @@
+# Load the libraries and set the local working envirnment where data is located
+
+setwd("~/Learning/03-GettingAndCleaningData/project/UCI HAR Dataset")
+library(data.table)
+library(dplyr)
+
+# Following steps are done as per the sequence mentioned in the assignment
+# Step 1: Merges the training and the test sets to create one data set.
+
